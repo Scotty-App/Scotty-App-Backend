0aaf987b8912c695e2cbe83bc8c81293384316c4
ManuelaPlanelles <manuelaplanelles@gmail.com>
2026-05-07 19:59:09 +0200

    Update MainController.java
    implemento la barra con los menus de acceso a las diferentes opciones

--- src/main/java/scottyapp/Main/MainController.java
+++ src/main/java/scottyapp/Main/MainController.java
@@ -1,15 +1,36 @@
 package scottyapp.Main;
 
-import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainController {
+public class MainApplication extends Application {
 
-    @FXML
-    public void irAPantalla2() throws IOException {
-        MainApplication.setRoot("scottyapp/SegundaPantallaController.java");
+    private static Scene scene;
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/scottyapp/main-view.fxml"));
+        scene = new Scene(fxmlLoader.load(), 1150, 840);
+        stage.setTitle("Gestor de Tablas - ScottyApp");
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public static void main(String[] args) {
+        launch();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
 }
